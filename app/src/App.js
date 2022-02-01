4fea6a30a748e0e7ea935e77cce04cf2fe68f923
high-g <altema1217bbfumi@live.jp>
2022-02-01 09:28:11 +0900

    feat: react-hook-form

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
 
 function App() {
+  const { register, handleSubmit, errors, isValid, getValues } = useForm({
+    defaultValues: { email: 'test@test.co.jp', password: 'aaaa' },
+  })
+
+  const onSubmit = (data) => {
+    console.log('data', data)
+  }
+
+  const onGet = (data) => {
+    console.log('get', getValues(['email', 'password']))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Email" name="email" ref={register()} />
+        {errors.email && 'パスワードは1文字以上、20文字以下でなければなりません。'}
+        <input type="password" placeholder="Password" name="password" ref={register} />
+        <input type="submit" value="submit" disabled={!isValid} />
+        <input type="button" value="確認" onClick={onGet} />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
